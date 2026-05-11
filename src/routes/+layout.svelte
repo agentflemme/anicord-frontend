e4e87d88483812fb27b9d450a5ddb3f4badfb215
orangc <c@orangc.net>
2026-05-11 10:18:02 +0300

    feat: setup tailwind and flyonui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-
+	import "../app.css";
 	let { children } = $props();
+
+  import { afterNavigate } from "$app/navigation";
+
+  afterNavigate(() => {
+    // Runs after navigating between pages
+    HSStaticMethods.autoInit();
+  });
 </script>
 
 <svelte:head>
