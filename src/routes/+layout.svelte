fab90e1eed3136ba91fbbadb5b371f75f0e58967
orangc <c@orangc.net>
2026-05-11 10:23:30 +0300

    fix: didn't import some stuff properly

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import { afterNavigate } from "$app/navigation";
 	import "../app.css";
 	let { children } = $props();
 
-  import { afterNavigate } from "$app/navigation";
-
-  afterNavigate(() => {
-    // Runs after navigating between pages
-    HSStaticMethods.autoInit();
-  });
+	afterNavigate(() => {
+		window.HSStaticMethods.autoInit();
+	});
 </script>
 
 <svelte:head>
